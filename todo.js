662c39e87c274d00a63ebb62733f3059503250d0
su <ojs9578@naver.com>
2021-01-17 21:46:30 +0900

    Added a function for deleting one from a to-do list
    Added a function for deleting one from a to-do list and changed newId as idNumbers += 1 due to bug of overlapped id

--- todo.js
+++ todo.js
@@ -4,7 +4,19 @@ const toDoform = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
-const toDos = [];
+let toDos = [];
+let idNumbers = 1;
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -14,9 +26,11 @@ function createToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
-    const newId = toDos.length + 1;
+    const newId = idNumbers;
+    idNumbers += 1;
     
     delBtn.innerText = "✖";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
