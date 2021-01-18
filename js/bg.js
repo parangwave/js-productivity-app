4213f40e91d10505d9b309ad390513f52a731102
su <ojs9578@naver.com>
2021-01-18 23:16:21 +0900

    Added a function for loading BG images randomly
    Added a part for loading BG images randomly

---
+++ js/bg.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body"),
+
+const IMG_NUMBER = 6;
+
+function genRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    console.log(number);
+    return number;
+}
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.PNG`;
+    image.classList.add('bgImage')
+    body.prepend(image);
+}
+
+function init() {
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
