dd2589a1905e14703e48d89d51e23780ebaecce6
su <ojs9578@naver.com>
2021-01-18 23:29:28 +0900

    Added a function for change font-color with changing BG image, edited css with reset.css, and replaced a image
    Added a function for change gradient font-color with changing BG image,
    added reset.css, edited css, grouped css files in a folder, and replaced a image(4.PNG)

--- js/bg.js
+++ js/bg.js
@@ -1,10 +1,13 @@
 const body = document.querySelector("body"),
+    h1 = document.querySelector("h1"),
+    h4 = document.querySelector("h4"),
+    li = document.querySelector("li"),
+    btn = document.querySelector("button");
 
 const IMG_NUMBER = 6;
 
 function genRandom() {
     const number = Math.floor(Math.random() * IMG_NUMBER);
-    console.log(number);
     return number;
 }
 
@@ -15,9 +18,24 @@ function paintImage(imgNumber) {
     body.prepend(image);
 }
 
+function changeFontColor(imgNumber) {
+    if ( (imgNumber + 1) < 5) {
+        h1.classList.remove("nightFont");
+        h4.classList.remove("nightFont");
+        li.classList.remove("nightFont");
+        btn.classList.remove("nightFont");
+    } else {
+        h1.classList.add("nightFont");
+        h4.classList.add("nightFont");
+        li.classList.add("nightFont");
+        btn.classList.add("nightFont");
+    }
+}
+
 function init() {
     const randomNumber = genRandom();
     paintImage(randomNumber);
+    changeFontColor(randomNumber);
 }
 
 init();
